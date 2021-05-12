0ee3c358528348cf8c06feb19776336e72f395ad
Sirvan Almasi <sirvan3tr@gmail.com>
2021-05-12 12:25:54 +0100

    undefined variable

--- jsutil.js
+++ jsutil.js
@@ -15,63 +15,64 @@ const listen = (el, event, func) => el.addEventListener(event, func);
 
 // createElement short version 
 const ce = (el, attrs={}, content=null, child=null) => {
-	const µ = document.createElement(el);
-	elContent(µ, content);
-	if (child) µ.appendChild(child);
+  const µ = document.createElement(el);
+  elContent(µ, content);
+  if (child) µ.appendChild(child);
   for (const [key, value] of Object.entries(attrs)) {
-		µ.setAttribute(key, value);}
-	return µ;}
+    µ.setAttribute(key, value);}
+  return µ;}
 
 /*
  * µ: element that we want to place the content in
  * ß: content of the element [atom, value]
  */
 const elContent = (µ, ß) => {
-	if (ß) {
-		if (ß[0].isAtom) {
-			µ.textContent = ß[0].getValue(ß[1]);	
-			contentListener(µ, ß[0], ß[1]);}
-    else { µ.textContent = ß;}}}
+  if (ß) {
+    if (ß[0].isAtom) {
+      µ.textContent = ß[0].getValue(ß[1]);	
+      contentListener(µ, ß[0], ß[1]);}
+  else { µ.textContent = ß;}}}
 
 /* Create a listener for our element */
 const contentListener = (obj, atom, newVal) => {
-	atom.listeners.add(obj);
-	listen(obj, atom.evName, (e) => {
-		obj.textContent = atom.getValue(newVal);});}
+  atom.listeners.add(obj);
+  listen(obj, atom.evName, (e) => {
+    obj.textContent = atom.getValue(newVal);});}
 
 // createElement but for a div
 const div = (attrs={}, content=null, child=null) =>
-	ce("div", attrs, content, child);
+  ce("div", attrs, content, child);
 
 class Atom {
-	constructor(value, eventName=null) {
-		this.state = value;
-		this.listeners = new Set();
-		this.eventName = eventName | Math.random().toString(36).substring(7);
-		this.ev = new CustomEvent(this.eventName, {
-		  detail: { hazcheeseburger: true }});}
+  constructor(value, eventName=null) {
+    this.state = value;
+    this.listeners = new Set();
+    this.eventName = eventName | Math.random().toString(36).substring(7);
+    this.ev = new CustomEvent(this.eventName, {
+      detail: { hazcheeseburger: true }});}
 
-	static name = "Atom";
+  static name = "Atom";
 
-	get isAtom() {
-		return true;}
+  get isAtom() {
+    return true;}
 
-	get event() {
-		return this.event;}
+  get event() {
+    return this.event;}
 
-	get evName() {
-		return this.eventName;}
+  get evName() {
+    return this.eventName;}
 
-	getValue(obj) {
-		return this.state[obj];}
+  getValue(obj) {
+    return this.state[obj];}
 
-	get val() {
-		return this.state;}	
+  get val() {
+    return this.state;}	
 
-	set(obj, newVal) {
-		this.state[obj] = newVal;
-		for(let µ of this.listeners) { µ.dispatchEvent(this.ev)}}}
+  set(obj, newVal) {
+    this.state[obj] = newVal;
+    for(let µ of this.listeners) { µ.dispatchEvent(this.ev)}}}
 
 // Keyword shortcuts
-const ƒ = ce;
-const δ = ce;
+const ƒ = ce,
+      δ = ce,
+      _ = undefined;
